c0821258fb8ccf40265a70547f397da6c69ae94e
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2022-12-31 11:44:14 +0100

    ready to deploy add user

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const User = require("./models/User");
 const Prooduct = require("./models/Product");
-
+const bcrypt = require("bcryptjs");
 const app = express();
+app.use(express.json());
 
 const router = express.Router();
 
@@ -19,6 +20,32 @@ app.get("/allProduct", async (req, res) => {
   const products = await Prooduct.find();
   res.json(products);
 });
+
+app.post("/add", async (req, res) => {
+  const user = new User(req.body);
+
+  const email = req.body.email;
+  const password = req.body.password;
+  const existe = await User.findOne({ email });
+
+  if (existe) {
+    return res.status(400).json("user existe");
+  }
+
+  const hashedPsw = await bcrypt.hash(password, 10);
+  const newUser = await User.create({
+    name: user.name,
+    email: user.email,
+    phone: user.phone,
+    password: hashedPsw,
+  });
+  return res.status(201).json(newUser);
+});
+
+const getUsers = async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+};
 
 app.listen(port, (err) => {
   err
